8dc65f10e88bc78114fcc543c3b636bfcb6f6f69
Jose Augusto Zevallos Ruiz <desarrollo.sistemas@ana.gob.pe>
2024-02-19 19:53:38 -0500

    update

--- scripts/PARAMETROS_MBLRP_OK.R
+++ scripts/PARAMETROS_MBLRP_OK.R
@@ -1,4 +1,6 @@
-source('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/src/InterpolationFunctions.R')
+source('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+       /src/InterpolationFunctions.R')
+
 library(raster)
 
 
@@ -6,17 +8,22 @@ grd=readRDS('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/data/grilla.rds')
 
 
 
-path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/parameters_jan.csv' 
+path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/CV_parameters/parameters_jan.csv' 
 mapas_mes(path_to_file = path_to_file,grd,month = 'jan')
 
-path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/parameters_feb.csv' 
+path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/CV_parameters/parameters_feb.csv' 
 mapas_mes(path_to_file = path_to_file,grd,month = 'feb')
 
-path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/parameters_mar.csv' 
+path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/CV_parameters/parameters_mar.csv' 
 mapas_mes(path_to_file = path_to_file,grd,month = 'mar')
 
-path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/parameters_jul.csv' 
+path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/CV_parameters/parameters_jul.csv' 
 mapas_mes(path_to_file = path_to_file,grd,month = 'jul')
 
-path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/parameters_ago.csv' 
+path_to_file='D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/CV_parameters/parameters_ago.csv' 
 mapas_mes(path_to_file = path_to_file,grd,month = 'ago')
--- scripts/VALIDACION_CRUZADA_GRAFICO.R
+++ scripts/VALIDACION_CRUZADA_GRAFICO.R
@@ -3,11 +3,13 @@ source('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/src/RMBLRP.R')
 source('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/src/plotHelpers.R')
 
 #Gauges statistics
-gauge_stats=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/data/gauge_stats_feb_filtered.csv')
+gauge_stats=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+                     /data/gauge_stats_feb_filtered.csv')
 
 
 #Model CV parameters
-validation_parameters=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_validation/CrossValidationParameters2.csv',sep =',')
+validation_parameters=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+                               /output/CV_validation/CrossValidationParameters2.csv',sep =',')
 
 #Simulated Stats
 simulated_stats=data.frame(gauge_stats[,c(1,2)],SimStats(validation_parameters[,-c(1,2)]))
@@ -17,8 +19,18 @@ simulated_stats=data.frame(gauge_stats[,c(1,2)],SimStats(validation_parameters[,
 #################################################################
 
 names(simulated_stats)=names(gauge_stats)[1:18]
-file_save_name="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/img/cv_stats_february.png"
-plot_comparison(gauge_stat = gauge_stat,simulated_stats = simulated_stats,file_save_name = file_save_name,language = 'English')
+file_save_name="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalizatio
+n/output/img/cv_stats_february.png"
 
-file_save_name="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/img/cv_estadisticos_febrero.png"
-plot_comparison(gauge_stat = gauge_stat,simulated_stats = simulated_stats,file_save_name = file_save_name,language = 'Spanish')
+plot_comparison(gauge_stat = gauge_stat,
+                simulated_stats = simulated_stats,
+                file_save_name = file_save_name,
+                language = 'English')
+
+file_save_name="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
+/output/img/cv_estadisticos_febrero.png"
+
+plot_comparison(gauge_stat = gauge_stat,
+                simulated_stats = simulated_stats,
+                file_save_name = file_save_name,
+                language = 'Spanish')

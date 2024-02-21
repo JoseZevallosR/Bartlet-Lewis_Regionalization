4a28dc3746fd226bc3ac68b25c0a0b21f04286a2
Jose Augusto Zevallos Ruiz <desarrollo.sistemas@ana.gob.pe>
2024-02-21 14:24:14 -0500

    aa

--- scripts/MBLRP_ESTIMACION.R
+++ scripts/MBLRP_ESTIMACION.R
@@ -7,15 +7,13 @@ opt='TRMM'
 
 if (opt=='TRMM'){
   #Mixed stats from gauge stations and corrected TRMM
-  gauge_stats=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
-                       /data/feb_gauge_stat.csv')
+  gauge_stats=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/data/feb_gauge_stat.csv')
   gauge_stats=kickOutliers(gauge_stats)
   gauge_stats=filter_Neigbors(gauge_stats)
   c(nrow,ncol) := dim(gauge_stats)
-  Lmin=matrix(c(0.1,0.001,0.001,0.001,0.0854,1),nrow = 6,ncol = nrow)
-  Lmax=matrix(c(4,0.1,0.1,0.1,0.1,20),nrow=6,ncol=nrow)
-  maps=run(rain_stats=gauge_stats,path="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
-           /output/CV_parameters/",iterations=5,Lmin,Lmax,FILE_NAME='parameters_ago.csv')
+  #Lmin=matrix(c(0.1,0.001,0.001,0.001,0.0854,1),nrow = 6,ncol = nrow)
+  #Lmax=matrix(c(4,0.1,0.1,0.1,0.1,20),nrow=6,ncol=nrow)
+  maps=run(rain_stats=gauge_stats,path="D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization/output/CV_parameters/",iterations=5,FILE_NAME='parameters_ago.csv')
 }else if(opt =='GPM'){
   #Mixed stats from gauge stations and corrected TRMM
   gauge_stats=read.csv('D:/Proyectos_GitHub/Bartlet-Lewis_Regionalization
